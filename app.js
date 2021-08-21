b614e78e22fdef6b2d4a3201f854e7b4763b6d50
Emmanuel Okafor <emmiko360@gmail.com>
2021-08-21 17:12:10 +0100

    Scrape all pages for today at intervals

--- app.js
+++ app.js
@@ -6,197 +6,149 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API is running...'));
 
-const url = 'http://www.flashscore.mobi/table-tennis/';
+const anchors = [
+  {href: 'http://www.flashscore.mobi/', title: 'Soccer'},
+  {href: 'http://www.flashscore.mobi/hockey/', title: 'Hockey'},
+  {href: 'http://www.flashscore.mobi/tennis/', title: 'Tennis'},
+  {href: 'http://www.flashscore.mobi/basketball/', title: 'Basketball'},
+  {href: 'http://www.flashscore.mobi/handball/', title: 'Handball'},
+  {href: 'http://www.flashscore.mobi/volleyball/', title: 'Volleyball'},
+  {href: 'http://www.flashscore.mobi/baseball/', title: 'Baseball'},
+  {href: 'http://www.flashscore.mobi/american-football/', title: 'American Football'},
+  {href: 'http://www.flashscore.mobi/rugby-union/', title: 'Rugby Union'},
+  {href: 'http://www.flashscore.mobi/aussie-rules/', title: 'Aussie Rules'},
+  {href: 'http://www.flashscore.mobi/badminton/', title: 'Badminton'},
+  {href: 'http://www.flashscore.mobi/bandy/', title: 'Bandy'},
+  {href: 'http://www.flashscore.mobi/beach-soccer/', title: 'Beach Soccer'},
+  {href: 'http://www.flashscore.mobi/beach-volleyball/', title: 'Beach Volleyball'},
+  {href: 'http://www.flashscore.mobi/boxing/', title: 'Boxing'},
+  {href: 'http://www.flashscore.mobi/cricket/', title: 'Cricket'},
+  {href: 'http://www.flashscore.mobi/darts/', title: 'Darts'},
+  {href: 'http://www.flashscore.mobi/field-hockey/', title: 'Field Hockey'},
+  {href: 'http://www.flashscore.mobi/floorball/', title: 'Floorball'},
+  {href: 'http://www.flashscore.mobi/futsal/', title: 'Futsal'},
+  {href: 'http://www.flashscore.mobi/kabaddi/', title: 'Kabaddi'},
+  {href: 'http://www.flashscore.mobi/mma/', title: 'MMA'},
+  {href: 'http://www.flashscore.mobi/netball/', title: 'Netball'},
+  {href: 'http://www.flashscore.mobi/rugby-league/', title: 'Rugby League'},
+  {href: 'http://www.flashscore.mobi/snooker/', title: 'Snooker'},
+  {href: 'http://www.flashscore.mobi/table-tennis/', title: 'Table Tennis'},
+  {href: 'http://www.flashscore.mobi/water-polo/', title: 'Water Polo'}
+];
 
-(async () => {
+const scrapeToday = async () => {
   try {
-    const response = await request(url);
-    const $ = cheerio.load(response);
-    const data = $('#score-data').html();
-    const segments = data.split('<h4>');
-    const tournaments = [];
-    segments.forEach((segment, index) => {
-      if (index === 0) return;
-      const title = segment.split('</h4>')[0];
-      const parts = segment.split('</h4>')[1];
-      const pieces = parts.split('<br>');
-      const tournament = {};
-      tournament.title = title;
-      tournament.matches = [];
-      console.log(title);
-      pieces.forEach((piece, index) => {
-        if (index === pieces.length - 1) return;
-        let time;
-        if (piece.includes('class="live"')) {
-          time = piece.match(/<span class="live">(.*)<\/span>/)[1];
-        } else if (piece.includes('class="status canceled"') || piece.includes('class="status"')) {
-          time = piece.match(/<span>(.*)<\/span>/)[1].split('<span')[0];
-        } else {
-          time = piece.match(/<span>(.*)<\/span>/)[1];
+    const sports = [];
+    for (let index = 0; index < anchors.length; index++) {
+      const anchor = anchors[index];
+      const response = await request({
+        uri: anchor.href,
+        method: 'GET',
+        headers: {
+          'Connection': 'keep-alive',
+          'Accept-Encoding': '',
+          'Accept-Language': 'en-US,en;q=0.8'
         }
-        let event = piece.match(/<\/span>(.*)<a/)[1].replace('</span>', '').trim();
-        let sideOne = event.split(' - ')[0];
-        let sideTwo = event.split(' - ')[1];
-        const redCardImage = /<img src="\/res\/image\/blank.gif" class="rcard-(.*)">/;
-        let sideOneRedCards, sideTwoRedCards;
-        if (sideOne.match(redCardImage)) {
-          sideOneRedCards = parseInt(sideOne.match(redCardImage)[1]);
-        } else {
-          sideOneRedCards = 0;
-        }
-        if (sideTwo.match(redCardImage)) {
-          sideTwoRedCards = parseInt(sideTwo.match(redCardImage)[1]);
-        } else {
-          sideTwoRedCards = 0;
-        }
-        event = sideOne.replace(redCardImage, '') + ' - ' + sideTwo.replace(redCardImage, '');
-        let status;
-        if (piece.includes('class="sched"')) {
-          status = 'Scheduled';
-        } else if (piece.includes('class="status canceled"')) {
-          status = 'Postponed';
-        } else if (piece.includes('class="status"')) {
-          status = 'Canceled';
-        } else if (piece.includes('class="live"')) {
-          status = 'Live';
-        } else if (piece.includes('class="fin"')) {
-          status = 'Finished';
-        }
-        const score = piece.match(/<a href="[\s\S]*" class="[\s\S]*">(.*)<\/a>/)[1];
-        let setsScore;
-        if (
-          piece.match(/<img src="\/res\/image\/blank.gif" class="tennis-serve">/) && 
-          piece.match(/<a href="[\s\S]*" class="live">/)
-        ) {
-          setsScore = piece.match(/<\/a> (.*)/)[1];
-        } else {
-          setsScore = '';
-        }
-        let sport;
-        switch (url) {
-          case 'http://www.flashscore.mobi/':
-            sport = 'Soccer';
-            break;
-          case 'http://www.flashscore.mobi/hockey/':
-            sport = 'Hockey';
-            break;
-          case 'http://www.flashscore.mobi/tennis/':
-            sport = 'Tennis';
-            break;
-          case 'http://www.flashscore.mobi/basketball/':
-            sport = 'Basketball';
-            break;
-          case 'http://www.flashscore.mobi/handball/':
-            sport = 'Handball';
-            break;
-          case 'http://www.flashscore.mobi/volleyball/':
-            sport = 'Volleyball';
-            break;
-          case 'http://www.flashscore.mobi/baseball/':
-            sport = 'Baseball';
-            break;
-          case 'http://www.flashscore.mobi/american-football/':
-            sport = 'American Football';
-            break;
-          case 'http://www.flashscore.mobi/rugby-union/':
-            sport = 'Rugby Union';
-            break;
-          case 'http://www.flashscore.mobi/aussie-rules/':
-            sport = 'Aussie Rules';
-            break;
-          case 'http://www.flashscore.mobi/badminton/':
-            sport = 'Badminton';
-            break;
-          case 'http://www.flashscore.mobi/bandy/':
-            sport = 'Bandy';
-            break;
-          case 'http://www.flashscore.mobi/beach-soccer/':
-            sport = 'Beach Soccer';
-            break;
-          case 'http://www.flashscore.mobi/beach-volleyball/':
-            sport = 'Beach Volleyball';
-            break;
-          case 'http://www.flashscore.mobi/boxing/':
-            sport = 'Boxing';
-            break;
-          case 'http://www.flashscore.mobi/cricket/':
-            sport = 'Cricket';
-            break;
-          case 'http://www.flashscore.mobi/darts/':
-            sport = 'Darts';
-            break;
-          case 'http://www.flashscore.mobi/field-hockey/':
-            sport = 'Field Hockey';
-            break;
-          case 'http://www.flashscore.mobi/floorball/':
-            sport = 'Floorball';
-            break;
-          case 'http://www.flashscore.mobi/futsal/':
-            sport = 'Futsal';
-            break;
-          case 'http://www.flashscore.mobi/kabaddi/':
-            sport = 'Kabaddi';
-            break;
-          case 'http://www.flashscore.mobi/mma/':
-            sport = 'MMA';
-            break;
-          case 'http://www.flashscore.mobi/netball/':
-            sport = 'Netball';
-            break;
-          case 'http://www.flashscore.mobi/rugby-league/':
-            sport = 'Rugby League';
-            break;
-          case 'http://www.flashscore.mobi/snooker/':
-            sport = 'Snooker';
-            break;
-          case 'http://www.flashscore.mobi/table-tennis/':
-            sport = 'Table Tennis';
-            break;
-          case 'http://www.flashscore.mobi/water-polo/':
-            sport = 'Water Polo';
-            break;
-          default:
-            break;
-        }
-        let day;
-        if (url.includes('?d=-1')) {
-          day = 'Yesterday';
-        } else if (url.includes('?d=1')) {
-          day = 'Tomorrow';
-        } else {
-          day = 'Today';
-        }
-        tournament.matches.push(
-          { 
-            sport,
-            time, 
-            event, 
-            score, 
-            sideOneRedCards, // Soccer only
-            sideTwoRedCards, // Soccer only
-            setsScore, // Tennis only
-            status,
-            day 
-          }
-        );
-        console.log(
-          sport,
-          time, 
-          event, 
-          score, 
-          sideOneRedCards, 
-          sideTwoRedCards,
-          setsScore, 
-          status,
-          day
-        );
       });
-      tournaments.push(tournament);
-    });
+      const $ = cheerio.load(response);
+      const data = $('#score-data').html();
+      const segments = data.split('<h4>');
+      const sport = {};
+      sport.name = anchor.title;
+      sport.tournaments = [];
+      console.log(sport.name);
+      for (let index = 0; index < segments.length; index++) {
+        if (index === 0) continue;
+        const segment = segments[index];
+        const title = segment.split('</h4>')[0];
+        let parts = segment.split('</h4>')[1];
+        if (parts.includes('Awaiting<br>updates')) {
+          parts = parts.replace(/Awaiting<br>updates/g, 'Awaiting updates');
+        }
+        const pieces = parts.split('<br>');
+        const tournament = {};
+        tournament.title = title;
+        tournament.matches = [];
+        console.log(tournament.title);
+        for (let index = 0; index < pieces.length; index++) {
+          if (index === pieces.length - 1) continue;
+          const piece = pieces[index];
+          let time;
+          if (piece.includes('class="live"')) {
+            time = piece.match(/<span class="live">(.*)<\/span>/)[1];
+          } else if (piece.includes('class="status canceled"') || piece.includes('class="status"')) {
+            time = piece.match(/<span>(.*)<\/span>/)[1].split('<span')[0];
+          } else {
+            time = piece.match(/<span>(.*)<\/span>/)[1];
+          }
+          let event = piece.match(/<\/span>(.*)<a/)[1].replace('</span>', '').trim();
+          const score = piece.match(/<a href="[\s\S]*" class="[\s\S]*">(.*)<\/a>/)[1];
+          let status;
+          if (piece.includes('class="sched"')) {
+            status = 'Scheduled';
+          } else if (piece.includes('class="status canceled"')) {
+            status = 'Postponed';
+          } else if (piece.includes('class="status"')) {
+            status = 'Canceled';
+          } else if (piece.includes('class="live"')) {
+            status = 'Live';
+          } else if (piece.includes('class="fin"')) {
+            status = 'Finished';
+          }
+          const data = {time, event, score, status};
+          if (anchor.title === 'Soccer') {
+            const sideOne = event.split(' - ')[0];
+            const sideTwo = event.split(' - ')[1];
+            let sideOneRedCards, sideTwoRedCards;
+            const redCardClass = 'rcard-';
+            if (sideOne.includes(redCardClass)) {
+              sideOneRedCards = parseInt(sideOne.match(/"rcard-(.*)"/)[1]);
+            } else {
+              sideOneRedCards = 0;
+            }
+            if (sideTwo.includes(redCardClass)) {
+              sideTwoRedCards = parseInt(sideTwo.match(/"rcard-(.*)"/)[1]);
+            } else {
+              sideTwoRedCards = 0;
+            }
+            const sideOneRedCardImage = `<img src="/res/image/blank.gif" class="rcard-${sideOneRedCards}">`;
+            const sideTwoRedCardImage = `<img src="/res/image/blank.gif" class="rcard-${sideTwoRedCards}">`;
+            data.event = sideOne.replace(sideOneRedCardImage, '') + ' - ' + sideTwo.replace(sideTwoRedCardImage, '');
+            data.sideOneRedCards = sideOneRedCards;
+            data.sideTwoRedCards = sideTwoRedCards;
+          }
+          if (anchor.title === 'Tennis') {
+            const sideOne = event.split(' - ')[0];
+            const sideTwo = event.split(' - ')[1];
+            const tennisServe = 'tennis-serve';
+            let setsScore;
+            if (
+              piece.match(/<img src="\/res\/image\/blank.gif" class="tennis-serve">/) && 
+              piece.match(/<a href="[\s\S]*" class="live">/)
+            ) {
+              setsScore = piece.match(/<\/a> (.*)/)[1];
+            } else {
+              setsScore = '';
+            }
+            const tennisServeImage = '<img src="/res/image/blank.gif" class="tennis-serve">';
+            data.event = sideOne.replace(tennisServeImage, '') + ' - ' + sideTwo.replace(tennisServeImage, '');
+            data.isSideOneTennisServe = sideOne.includes(tennisServe);
+            data.isSideTwoTennisServe = sideTwo.includes(tennisServe);
+            data.setsScore = setsScore;
+          }
+          console.log(data);
+          tournament.matches.push(data);
+        }
+        sport.tournaments.push(tournament);
+      }
+      sports.push(sport);
+    }
   } catch (error) {
     console.log(error);
   }
-})();
+};
+
+setInterval(scrapeToday, 120000); // 2 min.
 
 const port = 2000 || process.env.PORT;
 
